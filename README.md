43b3dcac372b191d53b0f8678dbe941e7b341b5c
SpellerSpoiler <pascal03angelo@gmail.com>
2025-02-27 09:04:44 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Dart-projects
+# Dart-projects
---
+++ lesson2/variables.dart
@@ -0,0 +1,7 @@
+void main(){
+  var name = "Paschal";
+  final city = "New York";
+  const diameter = "1000.1";
+
+  print('Name:$name, City:$city, Diameter:$diameter');
+}
